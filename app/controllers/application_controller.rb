aae944ffcfaef8805accc16de2b8febf3d985d8f
mashimo <mashita1023@gmail.com>
2020-03-05 15:40:21 +0900

    complete associate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::Base
     # protect_from_forgery with: :exception
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :current_user
 
+    def current_user
+        return unless session[:user_id]
+        @current_user ||= User.find(session[:user_id])
+    end
 protected
 
     def configure_permitted_parameters
